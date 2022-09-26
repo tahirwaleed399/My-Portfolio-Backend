024dad397964e3c96e7dea15195bcd03e6b3f494
Muhammad Waleed Tahir <73441359+tahirwaleed399@users.noreply.github.com>
2022-09-26 19:23:53 +0500

    another try

--- Routes/Contact.js
+++ Routes/Contact.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { contact, getContacts } from '../Controllers/Contact.js';
 import { isAuthenticatedUser } from '../Middlewares/auth.js';
+
+
 var router = express.Router();
 
 
 router.route('/contact').post(contact).get(isAuthenticatedUser,getContacts)
 
 
-export default router
+export default router;
